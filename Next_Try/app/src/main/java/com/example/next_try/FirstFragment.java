e8dfa3e03e383ec2d596731d3dcc80010a98fc5f
Midrododn <vitalijs.bekasovs@gmail.com>
2021-10-07 18:15:19 +0200

    grub from textbar

--- Next_Try/app/src/main/java/com/example/next_try/FirstFragment.java
+++ Next_Try/app/src/main/java/com/example/next_try/FirstFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,12 +14,14 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.example.next_try.databinding.FragmentFirstBinding;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class FirstFragment extends Fragment {
 
     private FragmentFirstBinding binding;
 
     private int count = 0;
+    //private String batTXT = new String();
 
     @Override
     public View onCreateView(
@@ -34,6 +37,7 @@ public class FirstFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         TextView msg = view.findViewById(R.id.textview_first);
+        EditText in_txt = view.findViewById(R.id.text_bar1);
 
         binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,9 +50,12 @@ public class FirstFragment extends Fragment {
         binding.buttonToast.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String batTXT = in_txt.getText().toString();
+                //batTXT = "Text example";
+                //batTXT = in_txt.getText().toString();
                 count++;
                 msg.setText("Button pressed " + count + " times.");
-                Toast myToast = Toast.makeText(getActivity(), "Test text!", Toast.LENGTH_SHORT);
+                Toast myToast = Toast.makeText(getActivity(), batTXT, Toast.LENGTH_SHORT);
                 myToast.show();
             }
         });

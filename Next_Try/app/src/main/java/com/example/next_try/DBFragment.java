38789b16071a21e8a500932508c0425838cec98c
Midrododn <vitalijs.bekasovs@gmail.com>
2021-10-08 03:39:29 +0200

    all works, DB no works

--- Next_Try/app/src/main/java/com/example/next_try/DBFragment.java
+++ Next_Try/app/src/main/java/com/example/next_try/DBFragment.java
@@ -1,5 +1,6 @@
 package com.example.next_try;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,20 +18,37 @@ public class DBFragment extends Fragment {
 
     private FragmentDBBinding binding;
 
-    private int iPlus = 0;
+    private int putToDB = 0;
     private LinkedList<String> varList = new LinkedList<String>();
+    private SQLiteDatabase myData ;
+
+    private void parseAndWrite (int var){
+        String par = Integer.toString(var);
+        String ins = "INSERT INTO vars_table VALUES ('" + par + "','" + par + "');";
+        myData.execSQL(ins);
+    }
+    public void generateDB(){
+        myData = SQLiteDatabase.openOrCreateDatabase("db",null);
+        String table;
+        table = "CREATE TABLE IF NOT EXISTS vars_table (" +
+                "   var_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "   var_name text NOT NULL);";
+        myData.execSQL(table);
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater , ViewGroup container,
             Bundle savedInstanceState
     ) {
+
         binding = FragmentDBBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
@@ -38,17 +56,43 @@ public class DBFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         TextView listDB = view.findViewById(R.id.textView_dbout);
+        TextView counterLabel = view.findViewById(R.id.label_page_3);
+        counterLabel.setText("0");
         varList.add("List from DB:\n");
+
+        binding.button3rdMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                putToDB--;
+                counterLabel.setText(Integer.toString(putToDB));
+            }
+        });
+
+        binding.button3rdPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                putToDB++;
+                counterLabel.setText(Integer.toString(putToDB));
+            }
+        });
 
         binding.button3rdAddtodb.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                varList.add("var\n");
-                String conv = varList.toString();
+            public void onClick(View view) {
+                String conv = Integer.toString(putToDB);
+                varList.add(conv);
+                conv = varList.toString();
                 conv = conv.replace("["," ");
                 conv = conv.replace(","," ");
                 conv = conv.replace("]"," ");
                 listDB.setText(conv);
+            }
+        });
+
+        binding.button3rdGendb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //generateDB();
             }
         });
     }

b82cbd35c947e92d4478eb92f1fca270a5e93536
Midrododn <vitalijs.bekasovs@gmail.com>
2021-10-09 10:48:19 +0200

    c++ android

--- CAndroid/app/src/main/cpp/new-lib.cpp
+++ CAndroid/app/src/main/cpp/new-lib.cpp
@@ -1,8 +1,42 @@
 #include <jni.h>
 #include <string>
+#include <stdio.h>
+#include "sqlite3.h"
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+
 
 extern "C" JNIEXPORT jint JNICALL
 Java_com_example_candroid_MainActivity_new_1int(JNIEnv *env, jobject thiz){
     std::int8_t ret_int = 11;
     return ret_int;
+}
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_candroid_MainActivity_prnt_1dir(
+            JNIEnv* env,
+            jobject /* this */) {
+        char location[100];
+        getcwd(location, 100);
+        std::string hello = "\nDir : ";
+        std::string tmp;
+        hello.append(location);
+
+        DIR *d;
+        struct dirent *dir;
+        d = opendir(".");
+
+        if (d){
+            while ((dir = readdir(d)) != NULL){
+                tmp = "\n";
+                tmp.append(dir ->d_name);
+                hello.append(tmp);
+            }
+            closedir(d);
+        }
+        return env->NewStringUTF(hello.c_str());
 }
--- CAndroid/app/src/main/java/com/example/candroid/MainActivity.java
+++ CAndroid/app/src/main/java/com/example/candroid/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.candroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.candroid.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
 
     // Used to load the 'candroid' library on application startup.
     static {
@@ -15,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private ActivityMainBinding binding;
+    private Button btn_gen = findViewById(R.id.btn_generatedb);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = binding.sampleText;
         String row = stringFromJNI() + " " + Integer.toString(ret_int()) +
-                " " + Integer.toString(new_int());
+                " " + Integer.toString(new_int()) + prnt_dir();
         tv.setText(row);
     }
 
@@ -37,4 +45,5 @@ public class MainActivity extends AppCompatActivity {
     public native String stringFromJNI();
     public native int ret_int();
     public native int new_int();
+    public native String prnt_dir();
 }
